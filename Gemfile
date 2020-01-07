111dd3505d67ba585142f0d9cb197c11e8677f01
touridev <ftorjob@gmail.com>
2020-01-07 17:00:12 -0300

    Atualizações chatbot Ruby Sinatra

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source :rubygems
+gem 'sinatra'
+gem "activerecord", '~> 5.2.2'
+gem "sinatra-activerecord"
+gem 'pg'
+gem 'rake'
+gem 'pg_search', '~> 2.0.1'
+  
+group :development do
+  gem "tux"
+end
+
+gem "factory_bot"
+gem "ffaker"
+gem 'database_cleaner'
+ 
+group :test do
+  gem "rack-test", require: "rack/test"
+  gem "rspec"
+end
---
+++ config/database.rb
@@ -0,0 +1,34 @@
+configure :test do
+    set :database, {
+      adapter: 'postgresql',
+      encoding: 'utf8',
+      database: 'onebitbot_test',
+      pool: 5,
+      username: 'postgres',
+      host: 'postgres'
+    }
+  end
+     
+  configure :development do
+    set :database, {
+      adapter: 'postgresql',
+      encoding: 'utf8',
+      database: 'onebitbot_development',
+      pool: 5,
+      username: 'postgres',
+      host: 'postgres'
+    }
+  end
+
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+   
+    set :database, {
+      adapter:  'postgresql',
+      host:     db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
