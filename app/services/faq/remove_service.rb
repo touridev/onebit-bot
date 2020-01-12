e5c1fa06917ab5e309de188efa89246efa0ed458
touridev <ftorjob@gmail.com>
2020-01-12 18:38:33 -0300

    teste de remover por id

--- app/services/faq/remove_service.rb
+++ app/services/faq/remove_service.rb
@@ -6,7 +6,7 @@ module FaqModule
       end
   
       def call
-        faq = Faq.find(id: @id)
+        faq = Faq.where(id: @id).last
         return "Questão inválida, verifique o Id" if faq == nil
         
         Faq.transaction do
